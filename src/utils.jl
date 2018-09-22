2cc02b60b2e8f62878585220c0ba4418199447da
pablofzr <pablofzr@ifisica.uaslp.mx>
2018-09-22 00:27:46 +0000

    Fixes for finding the parameter kappa for DHS

--- src/utils.jl
+++ src/utils.jl
@@ -9,7 +9,7 @@ dimension(sk::StructureFactor) = dimension(sk.u)
 """
     dhs_msa_parameter(T′, η::T, tol)
 
-Returns the solution `κ` to
+Returns the solution `κ` to (see [Wertheim1971])
 
 ```math
     \\frac{(1 + 4\\kappa\\eta)^2}{(1 - 2\\kappa\\eta)^4} -
@@ -18,21 +18,22 @@ Returns the solution `κ` to
 ```
 
 The solution is found by Newton's method
-(up to the specified tolerance `tol` for `κ * η`).
+(up to the specified tolerance `tol` for `ξ = κ * η`, when possible).
+
+[Wertheim1971] M. S. Wertheim, J. Chem. Phys. 55, 4291 (1971).
 """
 function dhs_msa_parameter(T′, η::T, tol) where {T <: AbstractFloat}
     y = 8η / T′
 
-    ξ = √(eps(T))
-    r = T(Inf)
+    ξ = 0.499 # 0 < ξ < 1 / 2
 
-    while abs(1 - r) > tol
-        ξ₀ = ξ
+    while true
+        ξ₋ = ξ
 
-        ξ₁ = 1 - 2ξ₀
-        ξ₂ = 1 +  ξ₀
-        ξ₃ = 1 + 4ξ₀
-        ξ₄ = 2 -  ξ₀
+        ξ₁ = 1 - 2ξ
+        ξ₂ = 1 +  ξ
+        ξ₃ = 1 + 4ξ
+        ξ₄ = 2 -  ξ
 
         ξ₁₂  = ξ₁ * ξ₂
         ξ₁₂⁴ = ξ₁₂^4
@@ -41,9 +42,16 @@ function dhs_msa_parameter(T′, η::T, tol) where {T <: AbstractFloat}
         ξ₃²  = ξ₃^2
         ξ₂⁶  = ξ₂⁴ * ξ₂^2
 
-        ξ = ξ₀ + (ξ₁₂ / 4) * (ξ₁⁶ - ξ₃² * ξ₂⁴ + y * ξ₁₂⁴) / (4ξ₃ * ξ₂⁶ + ξ₁⁶ * ξ₄)
+        ξ = ξ + (ξ₁₂ / 4) * (ξ₁⁶ - ξ₃² * ξ₂⁴ + y * ξ₁₂⁴) / (4ξ₃ * ξ₂⁶ + ξ₁⁶ * ξ₄)
 
-        r = ξ / ξ₀
+        r = ξ / ξ₋
+        
+        if (1 - r) ≤ tol
+            if r > 1
+                ξ = ξ₋
+            end
+            break
+        end
     end
 
     return ξ / η
